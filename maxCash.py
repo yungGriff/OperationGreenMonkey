f980ccf814825878d77421035b18b3895596d6ab
yungGriff <57887563+yungGriff@users.noreply.github.com>
2023-12-20 16:51:45 -0600

    Screen detector
    Implemented a screen detector to try increase the uptime of the program via circumventing in-game pop ups.

--- maxCash.py
+++ maxCash.py
@@ -3,6 +3,9 @@ import time
 import keyboard
 import datetime
 import win32api, win32con
+import cv2
+from PIL import ImageGrab
+import numpy as np
 
 
 # Function to click keys on the keyboard the specified number of times
@@ -11,24 +14,71 @@ def click_keys(keys, num_clicks):
         keyboard.press_and_release(keys)
         time.sleep(0.5)  # Adjust sleep time as needed
 
+# Function to click on the bad popup
+def click_on_bad_popup(match_location, template_size):
+    try:
+        # Extract the match location
+        click_x = match_location[0] + template_size[0] // 2
+        click_y = match_location[1] + template_size[1] // 2
+        pyautogui.click(click_x, click_y)
+        print(f"Clicked at ({click_x}, {click_y})")
+    except Exception as e:
+        print(f"Error while clicking: {e}")
+
+# Function to check if bad popup is present
+
+def is_bad_popup_present():
+    try:
+        # Capture screenshot using PIL
+        screenshot = ImageGrab.grab()
+        screenshot_np = np.array(screenshot)
+
+        # Load the template image
+        template = cv2.imread('testerPopUp.png', cv2.IMREAD_GRAYSCALE)
+
+        # Get template size
+        template_size = template.shape[:2]
+
+        # Convert images to grayscale
+        gray_screenshot = cv2.cvtColor(screenshot_np, cv2.COLOR_BGR2GRAY)
+
+        # Apply template matching
+        result = cv2.matchTemplate(gray_screenshot, template, cv2.TM_CCOEFF_NORMED)
+        _, max_val, _, max_loc = cv2.minMaxLoc(result)
+
+        # Set a threshold for considering a match
+        threshold = 0.7
+        if max_val >= threshold or max_val == 1.0:
+            return max_loc, template_size
+        else:
+            return None, None
+    except Exception as e:
+        print(f"Error in is_bad_popup_present: {e}")
+        return None, None
+
 
 
 # Session Counter Track the number of run throughs
 sessionCounter = 0
+bad_popup_check_counter = 0
+#Changing this value for testing... ATTENTION
+sessions_before_check = 5
+imageFinder_counter = 0
+bad_Checks_before_continue = 3
 # Monkey Monies Will only track session earnings
 monkeyMonies = 0
 # Hero coordinates
-hero_coords = (707, 308)
-# hero_coords = (573, 473)  -- End of the Road
-superMonkey_coords = (715, 234)
-# superMonkey_coords = (623, 429) -- End of the Road
-ninja_coords = (708, 368)
-# ninja_coords = (565, 523) -- End of the Road
+hero_coords = (734, 303)
+# hero_coords = (573, 473)  -- End of the Road (733, 266)
+superMonkey_coords = (741, 222)
+# superMonkey_coords = (623, 429) -- End of the Road 735, 234
+ninja_coords = (727, 368)
+# ninja_coords = (565, 523) -- End of the Road (727, 368)
 # levelingMonkey = (577, 597)
-levelingMonkey = (762, 368)
-gamePlay = (1532, 829)
+levelingMonkey = (855, 368)
+gamePlay = (1560, 819)
 gameHome = (792, 726)
-Next = (956, 765)
+Next = (979, 749)
 # starting the Game Coords.
 homePlay = (864, 780)
 Intermediate = (864, 780)
@@ -41,12 +91,40 @@ Deflation = (1168, 461)
 OK = (951, 664)
 while True:
     now = datetime.datetime.now()
-    # Delay before starting
-    time.sleep(3)
+    time.sleep(2)
     # Track the session activity
     sessionCounter = sessionCounter + 1
     print(sessionCounter)
     print(" ", now)
+
+    # Check for bad popup
+    if bad_popup_check_counter % sessions_before_check == 0:
+        try:
+          while imageFinder_counter < bad_Checks_before_continue:
+            match_location, template_size = is_bad_popup_present()
+            if match_location is not None:
+                print("Pop Up image found. Clicking on it.")
+                click_on_bad_popup(match_location, template_size)
+                time.sleep(.5)
+                pyautogui.click()
+                time.sleep(1)
+                imageFinder_counter += 1
+            else:
+                print("Pop up not found.")
+                break
+        except KeyboardInterrupt:
+            print("Recognition stopped")
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+        finally:
+            #reset the sessions counter
+            bad_popup_check_counter = 0
+            imageFinder_counter = 0
+            pyautogui.moveTo(*OK)
+            pyautogui.click(button='left')
+    bad_popup_check_counter += 1
+    #Delay before starting
+    time.sleep(3)
     # Select Super Monkey.
     click_keys('s', 1)
     time.sleep(1)
@@ -179,3 +257,4 @@ while True:
     pyautogui.click(button='left')
     time.sleep(5)
     pyautogui.click(button='left')
+
